76531b6314298b1a0d47278c2062d5e9cb6f80f2
jeemarcon <jessica.amarcon@gmail.com>
2018-06-05 22:07:43 -0300

    Update arvoreDecisao.py

--- arvoreDecisao.py
+++ arvoreDecisao.py
@@ -1,5 +1,9 @@
+from anytree import Node, RenderTree
+
 def ValorMaisComun(Dados, Target, Atributos):
-	MaisComum= ""  
+
+	MaisComum= ""
+  
 	ganhaMais = getNumeroAparicoes(dados, atributo, '>50K')
 	ganhaMenos = getNumeroAparicoes(dados, atributo, '<=50K')
 
@@ -12,6 +16,7 @@ def ValorMaisComun(Dados, Target, Atributos):
 
 
 def MelhorAtributo (Dados, Target, Atributos):
+	
 	Melhor = Atributos[0]
 	Ganho = 0
 	numAtributos = len(Atributos)
@@ -29,11 +34,47 @@ def MelhorAtributo (Dados, Target, Atributos):
 
 
 def ArvoreDecisao(Dados, Target, Atributos):
-	#Se Attributes for vazio, retorne o nó Root, com rótulo = valor mais comum de Target_Attribute em Examples
-	if len(Atributos) - 1 <= 0 
-	
-	Default = ValorMaisComum(Dados, Target, Atributos)
 
-	.
-	.
-	.
+	indice = getIndiceAtributo(dados,Target)
+	maior = ">50K"
+	menor = "<=50K"
+	imaior = 0
+	imenor = 0
+
+	a = ""
+
+	for i range(0,len(dados))
+		if imaior > 0 && imenor > 0:
+			break
+		else if Dados[i][indice] == maior
+			imaior ++
+		else if Dados[i][indice] == menor
+			imenor ++
+
+	#### se tudo for igual 
+	if imenor == len(dados):
+		return maior
+	else if imaior == len(dados)
+		return menor
+
+	##se acabarem os atributos
+	if len(Atributos) == 0:
+		return ValorMaisComum(Dados,Target,Atributos)
+	else 
+		a = MelhorAtributo(Dados, Target, Atributos)
+		Root = Node(a)
+
+	Valores_A = getValoresAtributo(Dados,a)
+
+	for y range (0,len(Valores_A))
+		Subconjunto = makeConjuntoATributo(dados,a,Valores_A[y])
+		if len(Subconjunto) == 0:
+			No = Node(ValorMaisComum(Dados,Target,Atributos), parent=Root)
+		else
+			ArvoreDecisao (Subconjunto,Target,Atributos.remove(a))
+
+	return No 
+
+
+
+	
