de0b6c90fe65779cfcdc2f4791cfdd8b4f704c57
Lucas <lucas.sacuman@gmail.com>
2018-07-20 18:52:32 -0300

    Podas e acuracias

--- arvoreDecisao.py
+++ arvoreDecisao.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 import pickle
 import random
 
-
+num_nos = []
 def ValorMaisComum(Dados, Target, Atributos):
 	MaisComum = ''
 	ganhaMais = calc.getNumeroAparicoes(Dados, Target, ">50K")	
@@ -13,7 +13,7 @@ def ValorMaisComum(Dados, Target, Atributos):
 	if ganhaMais > ganhaMenos:
 		MaisComum = '>50K'
 	else:
- 		MaisComum = '<=50K'
+		MaisComum = '<=50K'
 	return MaisComum
 
 
@@ -33,19 +33,20 @@ def MelhorAtributo (Dados,Atributos):
 	return Melhor
 
 def Excluir_vetor(vetor,atributo):
-    tam = len(vetor)
-    novoconjunto = []
-    for i in range(0, tam):
-        if vetor[i] != atributo:
-            novoconjunto.append(vetor[i])
-    return novoconjunto
+	tam = len(vetor)
+	novoconjunto = []
+	for i in range(0, tam):
+		if vetor[i] != atributo:
+			novoconjunto.append(vetor[i])
+	return novoconjunto
 
 
 
 def ArvoreDecisao(Dados, Target, Atributos,default):
 	Root = Node()
 
-
+	#global num_nos
+	#num_nos = num_nos + 1
 	indice = calc.getIndiceAtributo(Dados,Target)
 	maior = "<=50K"
 	menor = ">50K"
@@ -89,153 +90,155 @@ def ArvoreDecisao(Dados, Target, Atributos,default):
 			#print("Valor: " +Valores_A[y])
 			if len(Subconjunto) == 1:
 				#print(ValorMaisComum(Dados,Target,Atributos))
-				return Root	
+				return Root
 			else:
 				filhos[Valores_A[y]] = ArvoreDecisao(Subconjunto,Target,Excluir_vetor(Atributos,a),Root)
 				Root.label = Valores_A[y]
-                Root.filhos = filhos
-	    
-	return Root 
+				Root.filhos = filhos
+		
+	return Root
 
 def classificador(Dados,Arvore):
-    numeroExemplos = len(Dados)
-    numAcertos = 0
-    acuracia = 0
-    indiceClasse = calc.getIndiceAtributo(Dados,'X50k.year')
-    for i in range(1,len(Dados)):
-        classeExemplo = Dados[i][indiceClasse]
-        classeReal = avaliaExemplo(Arvore, Dados[i], Dados)
-        if classeExemplo == classeReal:
-            numAcertos += 1
-        acuracia = numAcertos/(numeroExemplos-1)
-    #print("A acuracia foi de :",acuracia)
-    return acuracia
+	numeroExemplos = len(Dados)
+	numAcertos = 0
+	acuracia = 0
+	indiceClasse = calc.getIndiceAtributo(Dados,'X50k.year')
+	for i in range(1,len(Dados)):
+		classeExemplo = Dados[i][indiceClasse]
+		classeReal = avaliaExemplo(Arvore, Dados[i], Dados)
+		if classeExemplo == classeReal:
+			numAcertos += 1
+		acuracia = numAcertos/(numeroExemplos-1)
+	#print("A acuracia foi de :",acuracia)
+	return acuracia
 
 
 def avaliaExemplo(NoRaiz, Exemplo, Dados):
-    nosVisitados = 0
-    while len(NoRaiz.filhos) != 0:
-        atributoNo = NoRaiz.atributo
-        indiceAtributo = calc.getIndiceAtributo(Dados, atributoNo)
-        valorAtributoExemplo = Exemplo[indiceAtributo]
-        valorAtributoAux =""
-        for a in NoRaiz.filhos:
-           if valorAtributoAux == "":
-              valorAtributoAux = a 
-           elif valorAtributoExemplo == a:
-                valorAtributoAux = a
-        NoRaiz = NoRaiz.filhos[valorAtributoAux]
-        nosVisitados += 1
-        if NoRaiz == '<=50K' or NoRaiz == '>50K':
-            return NoRaiz
-    return NoRaiz
+	nosVisitados = 0
+	while len(NoRaiz.filhos) != 0:
+		atributoNo = NoRaiz.atributo
+		indiceAtributo = calc.getIndiceAtributo(Dados, atributoNo)
+		valorAtributoExemplo = Exemplo[indiceAtributo]
+		valorAtributoAux =""
+		for a in NoRaiz.filhos:
+		   if valorAtributoAux == "":
+			  valorAtributoAux = a 
+		   elif valorAtributoExemplo == a:
+				valorAtributoAux = a
+		NoRaiz = NoRaiz.filhos[valorAtributoAux]
+		nosVisitados += 1
+		if NoRaiz == '<=50K' or NoRaiz == '>50K':
+			return NoRaiz
+	return NoRaiz
 
 
 def getRegras(No):
-    regras = []
-    regraAteEntao = []
-    getRegrasRec(No, regraAteEntao, regras, 0)
+	regras = []
+	regraAteEntao = []
+	getRegrasRec(No, regraAteEntao, regras, 0)
 
 
 def getRegrasRec(No, RegraAteEntao, regras, Indice):
-    if not No:
-        return []
-    else:
-        while len(No.filhos) != 0:
-            aux = [No.atributo, No.filhos[Indice]]
-            RegraAteEntao.append(aux)
-            if No == '<=50K' or No == '>50K':
-                RegraAteEntao.append(No)
-                regras.append(RegraAteEntao)
-                indice += 1
-                return
-            getRegrasRec(No.filhos[Indice], RegraAteEntao, regras, Indice)
+	if not No:
+		return []
+	else:
+		while len(No.filhos) != 0:
+			aux = [No.atributo, No.filhos[Indice]]
+			RegraAteEntao.append(aux)
+			if No == '<=50K' or No == '>50K':
+				RegraAteEntao.append(No)
+				regras.append(RegraAteEntao)
+				indice += 1
+				return
+			getRegrasRec(No.filhos[Indice], RegraAteEntao, regras, Indice)
 
 def compara_acuracia (Root, Dados):
-    acuraciaAntiga = classificador(Dados,Root)
-    novoNo = deepcopy(Root)
-    if len(novoNo.filhos) != 0:
-        novoNo.filhos = {}
-        acuraciaNova = classificador(Dados,novoNo)
-        if acuraciaNova > acuraciaAntiga:
-            Root.filhos = {}
-            return Root
-    return Root
+	acuraciaAntiga = classificador(Dados,Root)
+	novoNo = deepcopy(Root)
+	if len(novoNo.filhos) != 0:
+		novoNo.filhos = {}
+		acuraciaNova = classificador(Dados,novoNo)
+		if acuraciaNova > acuraciaAntiga:
+			Root.filhos = {}
+			return Root
+	return Root
 
 def comp_acc (Root, no, Dados, ocorrencia):
-    acuraciaAntiga = classificador(Dados,Root)
-    #no_file = open(r'no_file.pkl','wb')
-    #pickle.dump(no,no_file)
-    #no_file.close()
-    filhos = no.filhos[no.label]
-    #no.label = ocorrencia
-    no.filhos[no.label] = ocorrencia
-    #no=ocorrencia
-    acuraciaNova = classificador(Dados,Root)
-    if acuraciaNova > acuraciaAntiga:
-        print(acuraciaNova)
-        return Root
-    elif acuraciaNova == acuraciaAntiga:
-        return Root
-    else:
-        #recover_file = open(r'no_file.pkl','rb')
-        #no_recover = pickle.load(recover_file)
-        #recover_file.close()
-        #Root = no_recover
-        ##no.filhos = no_recover.filhos
-        no.filhos[no.label] = filhos
-        return Root
+	global num_nos
+	acuraciaAntiga = classificador(Dados,Root)
+	num_nos.append(acuraciaAntiga)
+	#no_file = open(r'no_file.pkl','wb')
+	#pickle.dump(no,no_file)
+	#no_file.close()
+	filhos = no.filhos[no.label]
+	#no.label = ocorrencia
+	no.filhos[no.label] = ocorrencia
+	#no=ocorrencia
+	acuraciaNova = classificador(Dados,Root)
+	if acuraciaNova > acuraciaAntiga:
+		print(acuraciaNova)
+		return Root
+	elif acuraciaNova == acuraciaAntiga:
+		return Root
+	else:
+		#recover_file = open(r'no_file.pkl','rb')
+		#no_recover = pickle.load(recover_file)
+		#recover_file.close()
+		#Root = no_recover
+		##no.filhos = no_recover.filhos
+		no.filhos[no.label] = filhos
+		return Root
 
 def poda2(Root, no, Dados, sinaliza_root):
-    menor = "<=50K"
-    maior = ">50K"
-    ocorrencia = ''
-    if len(no.filhos) != 0:
-        #cont_folha = 0
-        cont_menor = 0
-        cont_maior = 0
-        add_menor = 0
-        add_maior = 0
-        for i in no.filhos:
-            if no.filhos[i] == menor:
-                cont_menor +=1
-            elif no.filhos[i] == maior:
-                cont_maior +=1
-            else:
-                add_menor, add_maior = poda2(Root,no.filhos[i],Dados,1)
+	menor = "<=50K"
+	maior = ">50K"
+	ocorrencia = ''
+	if len(no.filhos) != 0:
+		#cont_folha = 0
+		cont_menor = 0
+		cont_maior = 0
+		add_menor = 0
+		add_maior = 0
+		for i in no.filhos:
+			if no.filhos[i] == menor:
+				cont_menor +=1
+			elif no.filhos[i] == maior:
+				cont_maior +=1
+			else:
+				add_menor, add_maior = poda2(Root,no.filhos[i],Dados,1)
 
-        cont_menor += add_menor
-        cont_maior += add_maior
-        #cont_folha = cont_menor + cont_maior
-        #if ocorrencia=='':
-        if cont_menor > cont_maior:
-            #no.recorrente = menor
-            Root = comp_acc(Root, no, Dados, menor) #Representa maioria "<=50K"
-        elif cont_maior > cont_menor:
-            #no.recorrente = maior
-            Root = comp_acc(Root,no,Dados,maior) #Representa maioria ">50K" talvez enviese o modelo
-        else:
-            randomiza = random.choice([menor, maior])
-            #no.recorrente = randomiza
-            Root = comp_acc(Root,no,Dados,randomiza)
+		cont_menor += add_menor
+		cont_maior += add_maior
+		#cont_folha = cont_menor + cont_maior
+		#if ocorrencia=='':
+		if cont_menor > cont_maior:
+			#no.recorrente = menor
+			Root = comp_acc(Root, no, Dados, menor) #Representa maioria "<=50K"
+		elif cont_maior > cont_menor:
+			#no.recorrente = maior
+			Root = comp_acc(Root,no,Dados,maior) #Representa maioria ">50K" talvez enviese o modelo
+		else:
+			randomiza = random.choice([menor, maior])
+			#no.recorrente = randomiza
+			Root = comp_acc(Root,no,Dados,randomiza)
 
-        if sinaliza_root == 1:
-            return cont_menor, cont_maior
-
-    return Root
+		if sinaliza_root == 1:
+			return cont_menor, cont_maior
+	global num_nos
+	return Root, num_nos
 
 def poda (Root, Dados):
-    temp = []
-    temp.append(Root)
-    while len(temp) != 0:
-        n = temp.pop(0)	
-        n = compara_acuracia(n, Dados)
-        filhos = n.filhos
-        if len(filhos) != 0:
-            for i in filhos:
-                if filhos[i] != '<=50K' and filhos[i] != '>50K':
-                     temp.append(filhos[i])
-    return Root
+	temp = []
+	temp.append(Root)
+	while len(temp) != 0:
+		n = temp.pop(0)	
+		n = compara_acuracia(n, Dados)
+		filhos = n.filhos
+		if len(filhos) != 0:
+			for i in filhos:
+				if filhos[i] != '<=50K' and filhos[i] != '>50K':
+					 temp.append(filhos[i])
+	return Root
 
 
 
--- exec.py
+++ exec.py
@@ -25,6 +25,12 @@ treinamento = pd.read_csv('cjid3_0.txt', sep=',', header = None)
 treinamento = treinamento.values
 
 Root = decisao.ArvoreDecisao(treinamento,'X50k.year',treinamento[0],0)
+#print("num_nos:", num_nos)
+
+#num_nos_file = open('num_nos.txt', 'wb')
+#pickle.dump(num_nos, num_nos_file)
+#num_nos_file.close()
+
 
 root_train_file = open(r'root_train.pkl','wb')
 pickle.dump(Root,root_train_file)
@@ -41,13 +47,19 @@ test = pd.read_csv('cjid3_1.txt', sep=',', header = None)
 test = test.values
 
 acuracia_antes = decisao.classificador(test, Root)
-Root = decisao.poda2(Root,Root,validacao,0)
+
+Root, lista_acuracias = decisao.poda2(Root,Root,validacao,0)
+
+with open('lista_acuracias.txt','wb') as lista_acuracias_file:
+    for a in lista_acuracias:
+        lista_acuracias_file.write(str(a)+'\n')
+
+
 acuracia_depois = decisao.classificador(test, Root)
 
 root_poda_file = open(r'root_poda.pkl','wb')
 pickle.dump(Root,root_poda_file)
 root_poda_file.close()
-
 
 f1 = open('acuracia_pre_poda.txt', 'wb')
 #f1.write(acuracia_antes)
@@ -59,5 +71,49 @@ f2 = open('acuracia_pos_poda.txt', 'wb')
 pickle.dump(acuracia_depois, f2)
 f2.close()
 
-print("Acuracia antes da poda: %f",  acuracia_antes)
-print("Acuracia depois da poda: %f",  acuracia_depois)
+
+
+
+
+root_2_file = open(r'root_train.pkl','rb')
+Root2 = pickle.load(root_2_file)
+root_2_file.close()
+
+Root2, lista_acuracias2 = decisao.poda2(Root2,Root2,test,0)
+
+root_poda2_file = open(r'root_poda2.pkl','wb')
+pickle.dump(Root2,root_poda2_file)
+root_poda2_file.close()
+
+
+with open('lista_acuracias_2.txt','wb') as lista_acuracias_2_file:
+    for a in lista_acuracias2:
+        lista_acuracias_2_file.write(str(a)+'\n')
+
+
+
+
+
+root_3_file = open(r'root_train.pkl','rb')
+Root3 = pickle.load(root_3_file)
+root_3_file.close()
+
+treinamento_poda = pd.read_csv('cjid3_0.txt', sep=',', header = None)
+treinamento_poda = treinamento_poda.values
+
+Root3, lista_acuracias3 = decisao.poda2(Root3,Root3,treinamento_poda,0)
+
+root_poda3_file = open(r'root_poda3.pkl','wb')
+pickle.dump(Root3,root_poda3_file)
+root_poda3_file.close()
+
+
+with open('lista_acuracias_3.txt','wb') as lista_acuracias_3_file:
+    for a in lista_acuracias3:
+        lista_acuracias_3_file.write(str(a)+'\n')
+
+
+
+
+#print("Acuracia antes da poda: %f",  acuracia_antes)
+#print("Acuracia depois da poda: %f",  acuracia_depois)
